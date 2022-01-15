4c563a0cf9c11d82c50ecc757b4f3a55945f19e0
Thomas <65050898+tvonment@users.noreply.github.com>
2022-01-15 19:04:46 +0000

    impl custom vision

--- dialogs/handleAttachementDialog.js
+++ dialogs/handleAttachementDialog.js
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 const { ComponentDialog, WaterfallDialog } = require('botbuilder-dialogs');
+const axios = require('axios');
 
 const HANDLE_ATTACHEMENT_DIALOG = 'HANDLE_ATTACHEMENT_DIALOG';
 
@@ -13,6 +14,7 @@ class HandleAttachementDialog extends ComponentDialog {
 
         this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
             this.initStep.bind(this),
+            this.sendToCustomVisionStep.bind(this),
             this.finalStep.bind(this)
         ]));
 
@@ -22,6 +24,31 @@ class HandleAttachementDialog extends ComponentDialog {
     async initStep(stepContext) {
         // Continue using the same selection list, if any, from the previous iteration of this dialog.
         await stepContext.context.sendActivity('Attachement Dialog fired.');
+        return await stepContext.next();
+    }
+
+    async sendToCustomVisionStep(stepContext) {
+        console.log(stepContext.context.activity.attachments[0].content)
+
+        await axios({
+            method: 'post',
+            url: 'https://tobomedcustomvision-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/e489f41e-bfd4-41aa-b2b8-addf90b8789a/classify/iterations/Iteration1/url',
+            headers: {
+                "Prediction-Key": process.env.CustomVisionKey,
+                "Content-Type": "application/json"
+            },
+            data: {
+                Url: stepContext.context.activity.attachments[0].content.downloadUrl
+            }
+        })
+            .then(async (response) => {
+                console.log(response);
+                await stepContext.context.sendActivity(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
         return await stepContext.next();
     }
 
